aa1747070cf613357cfe74268bd074da560728fe
Lindani-Zwide <222034990@stu.ukzn.ac.za>
2024-12-13 22:27:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+
+const characters = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", 
+    "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", 
+    "?", "/"
+  ];
+  
+  let password = [];
+  const passwordLengthInput = document.getElementById("password-length");
+  const generatedPasswordElement = document.getElementById("generated-password");
+  let isPasswordSet=false
+  function generatePassword() {
+    const passwordLength = parseInt(passwordLengthInput.value);
+    if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 15) {
+      generatedPasswordElement.textContent = "Invalid password length.Enter a value between 8 and 15(inclusive).";
+      generatedPasswordElement.style.color = "red";
+      return;
+    }
+  
+    password = [];
+    for (let i = 0; i < passwordLength; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      const randomCharacter = characters[randomIndex];
+      password.push(randomCharacter);
+    }
+  
+    generatedPasswordElement.textContent = password.join("");
+    generatedPasswordElement.style.color = "";
+    isPasswordSet=true
+  }
+
+  
+function copyToClipboard() {
+    if(isPasswordSet){
+        const generatedPassword = document.getElementById("generated-password").textContent;
+        navigator.clipboard.writeText(generatedPassword).then(() => {
+          document.getElementById("generated-password").textContent = "Password copied to clipboard!";
+          document.getElementById("generated-password").style.color = "green";
+          setTimeout(() => {
+            document.getElementById("generated-password").textContent = generatedPassword;
+            document.getElementById("generated-password").style.color = "";
+          }, 2000);
+        }).catch((error) => {
+          document.getElementById("generated-password").textContent = "Error copying password to clipboard: " + error;
+          document.getElementById("generated-password").style.color = "red";
+          setTimeout(() => {
+            document.getElementById("generated-password").textContent = generatedPassword;
+            document.getElementById("generated-password").style.color = "";
+          }, 2000);
+        });
+    }
+    isPasswordSet=false
+    
+  }
+  
+  
+  
